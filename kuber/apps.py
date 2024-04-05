0c6ef92b84f34e26c922e7f338a2befee367a086
Deepak van <65806399+deepakvan@users.noreply.github.com>
2024-04-05 10:44:21 +0530

    Update apps.py

--- kuber/apps.py
+++ kuber/apps.py
@@ -1,9 +1,13 @@
 from django.apps import AppConfig
-
+import threading
 
 class KuberConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'kuber'
+
+def ready(self):
+    from .views import bot
+    threading.Thread(target=bot).start()
 
 
 
